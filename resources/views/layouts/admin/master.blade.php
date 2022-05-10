27fd7e22439a9841d36f36141721399ad1d644e9
dipta995 <dipta995@gmail.com>
2022-05-11 01:20:02 +0600

    review complete

--- resources/views/layouts/admin/master.blade.php
+++ resources/views/layouts/admin/master.blade.php
@@ -111,10 +111,12 @@
                                 <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
                                 Customers
                             </a>
-                            <a class="nav-link" href="tables.html">
-                                <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
-                                Tables
+                            <div class="sb-sidenav-menu-heading">Addons</div>
+                            <a class="nav-link" href="{{ url('admin/reviews') }}">
+                                <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
+                                Reviews
                             </a>
+
                         </div>
                     </div>
                     <div class="sb-sidenav-footer">
